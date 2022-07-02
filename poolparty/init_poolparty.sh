dbf9c6097a46470846fb9ab88113131498b628de
sampocs <sam.pochyly@gmail.com>
2022-07-02 13:57:31 -0400

    added check for invalid node names

--- poolparty/init_poolparty.sh
+++ poolparty/init_poolparty.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-# set -e
+#!/bin/sh
+set -e
 clear 
 
 PURPLE='\033[0;35m'
@@ -13,10 +13,12 @@ printf "This script will guide you through setting up your very own Stride node 
 
 printf "First, we need to give your node a nickname. "
 
+node_name_prompt="What would you like to call it? "
 while true; do
-    read -p "$(echo $PURPLE"What would you like to call it? "$NC)" NODE_NAME
-    if [ -z "$NODE_NAME" ]; then
-    printf 'Please enter a node name.'
+    read -p "$(echo $PURPLE"$node_name_prompt"$NC)" NODE_NAME
+    if [ -z "$NODE_NAME" ] || ! [[ "$NODE_NAME" =~ ^[A-Za-z0-9-]*$ ]]; then
+        printf '\nNode names can only container letters, numbers, and hyphens.\n'
+        node_name_prompt="Please enter a new name. "
     else
         break
     fi
