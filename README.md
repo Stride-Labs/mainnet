e5721cb0e1c049ea4be9cacabca10e7adc7bea83
vish-stride <vishal@stridelabs.co>
2022-07-02 02:39:56 -0400

    touches to init script and readme

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# testnet
-Instructions to connect to Stride's testnet
+# Stride Testnet
+
+This contains instructions for how to connect to Stride's testnet, PoolParty!
+
+We've tried to keep the instructions as simple as possible, but if you have any questions, please don't hesitate to contact us over [email](mailto:hello@stridelabs.co), on [Twitter](https://twitter.com/stride_zone), or at our [Discord](https://stride.zone/discord).
+
+Requirements for this testnet are quite minimal. You should have a 4 CPU 32 GB RAM machine. We've only tested this build on OSX and Linux machines, but we hope to support Windows soon.
+
+## Installation
+
+Run the following command to install PoolParty
+
+```
+sh -c "$(curl -sSL install.poolparty.stridelabs.co)"
+```
+
+## Running Stride Commands
+
+Please add the repo with your stride binary (defaults to `$HOME/go/bin`) to your `.bashrc` or `.zshrc` file. Then, you can run `strided` automatically.
+
+For example, run `strided q bank balances stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq` to see the bank balance of this address.
+
+Another, practical example, if you run 
+
+    strided keys list
+
+you can see your local keys. If you wish to make a new account, please run 
+
+    strided keys add <NAME>
+
+If you run `strided q help`, you'll see a list of potential options. Some useful flows might be 
+
+    strided tx stakeibc liquid-stake
+    strided tx stakeibc redeem-stake
+
+## Being a Validator
+
+If you want to convert your node into a validator, please run 
+
+    strided tx staking create-validator
+
+and follow the prompts.
+
+## FAQ
+
+### Where can I get some tokens?
+
+To get tokens, message `$faucet {{ADDRESS}}` on Discord. For example, I would type `$fauct stride159atdlc3ksl50g0659w5tq42wwer334ajl7xnq`. Please don't spam this.
+
+###  How can I relaunch my Node?
+
+When you first install your testnet, you should have seen two `sh` commands printed at the end. These contain instructions for how to connect to your testnet. 
+
+If you used the presets, you should be able to run the following command to relaunch your node. 
+
+``` strided start```
+or
+``` sh $HOME/.stride/poolparty/launch_poolparty.sh```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change. We also welcome all discussion in #engineering or #questions in our [Discord](https://stride.zone/discord). 
--- poolparty/init_poolparty.sh
+++ poolparty/init_poolparty.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-
+# set -e
 clear 
 
+PURPLE='\033[0;35m'
+BOLD="\033[1m"
+BLUE='\033[1;34m'
+ITALIC="\033[3m"
+NC="\033[0m"
+
+printf "\n\n${BOLD}Welcome to the setup script for Stride's Testnet, ${PURPLE}PoolParty${NC}!\n\n"
+printf "This script will guide you through setting up your very own Stride node locally.\n\n"
+
+printf "First, we need to give your node a nickname. "
+
 while true; do
-    read -p "Please type in name for your local node: " NODE_NAME
+    read -p "$(echo $PURPLE"What would you like to call it? "$NC)" NODE_NAME
     if [ -z "$NODE_NAME" ]; then
-    echo 'Please enter a node name.'
+    printf 'Please enter a node name.'
     else
         break
     fi
@@ -14,16 +25,21 @@ done
 # curl https://raw.githubusercontent.com/Stride-Labs/testnet/main/poolparty/init_poolparty.sh | sh
 TESTNET="internal"
 INSTALL_FOLDER="$HOME/.stride/$TESTNET"
-STRIDE_FOLDER="$INSTALL_FOLDER/stride"
+STRIDE_FOLDER="$HOME/.stride"
 
-mkdir -p $STRIDE_FOLDER
-cd $INSTALL_FOLDER
-echo "Installing to $STRIDE_FOLDER\n"
+BLINE="\n${BLUE}===============================================================================${NC}\n"
+printf $BLINE
+
+printf "\nGreat, now we'll download the latest version of Stride.\n"
+printf "Stride will keep track of blockchain state in ${BOLD}$STRIDE_FOLDER${NC}\n\n"
 
 if [ -d $STRIDE_FOLDER ] 
 then
+    printf "${BOLD}Looks like you already have Stride installed.${NC}\n"
+    printf "Proceed carefully, because you won't be able to recover your data if you overwrite it.\n\n"
+    pstr="Do you want to overwrite your existing $TESTNET installation and proceed? [y/n] "
     while true; do
-        read -p "Do you want to overwrite your existing $TESTNET blockchain and reinitialize? [y/n] " yn
+        read -p "$(echo $PURPLE"$pstr"$NC)" yn
         case $yn in
             [Yy]* ) break;;
             [Nn]* ) exit;;
@@ -31,40 +47,66 @@ then
         esac
     done
 fi
+printf $BLINE
 
 rm -rf $STRIDE_FOLDER > /dev/null 2>&1
-rm launch_testnet.sh > /dev/null 2>&1
+rm -f launch_testnet.sh > /dev/null 2>&1
 
-echo "\nPulling Stride repo..."
+mkdir -p $INSTALL_FOLDER
+cd $INSTALL_FOLDER
+
+echo "\nFetching Stride's code...\n\n"
 git clone https://github.com/Stride-Labs/stride.git > /dev/null 2>&1
 cd stride 
 git checkout 62e897c34f66d9cd0a7e0307517cd41c55a8f473 > /dev/null 2>&1
 
-mkdir $STRIDE_FOLDER/build/
-mkdir $STRIDE_FOLDER/build/stride/
+mkdir -p $INSTALL_FOLDER/build/
 
-make local-build
-STRIDE_HOME=$STRIDE_FOLDER/build/stride/
-echo "Initializing chain..."
+# pick install location
+DEFAULT_BINARY="$HOME/go/bin"
+rstr="Where do you want to install your stride binary? [default: $DEFAULT_BINARY] "
+read -p "$(echo Almost there\! $PURPLE"$rstr"$NC)" BINARY_LOCATION
+if [ -z "$BINARY_LOCATION" ]; then
+    BINARY_LOCATION=$DEFAULT_BINARY
+fi
+printf "\n"
+mkdir -p $BINARY_LOCATION
+sh $INSTALL_FOLDER/stride/scripts-local/build.sh -s $BINARY_LOCATION
+printf "\n"
 
-$STRIDE_FOLDER/build/strided init $NODE_NAME --home $STRIDE_HOME --chain-id STRIDE --overwrite > /dev/null 2>&1
+printf $BLINE
+
+BINARY=$BINARY_LOCATION/strided
+printf "\nLast step, now we need to setup your genesis state to match PoolParty...\n"
+
+$BINARY init $NODE_NAME --home $STRIDE_FOLDER --chain-id STRIDE --overwrite > /dev/null 2>&1
 
 # Now pull the genesis file
-curl https://bafkreid3dxxitn75gwr6dllhouasrdzgwmlj6wcyvrbspzzlsi453iskye.ipfs.dweb.link/ -o $STRIDE_FOLDER/build/stride/config/genesis.json > /dev/null 2>&1
+curl https://bafkreid3dxxitn75gwr6dllhouasrdzgwmlj6wcyvrbspzzlsi453iskye.ipfs.dweb.link/ -o $STRIDE_FOLDER/config/genesis.json > /dev/null 2>&1
 
 # # add persistent peer
-config_path="$STRIDE_FOLDER/build/stride/config/config.toml"
+config_path="$STRIDE_FOLDER/config/config.toml"
 PEER_ID="2ccf88e4c18e072f6173f80392ed5e61fccaf719@stride-node1.internal.stridenet.co:26656"
 sed -i -E "s|persistent_peers = \".*\"|persistent_peers = \"$PEER_ID\"|g" $config_path
 
-fstr="$STRIDE_FOLDER/build/strided start --home $STRIDE_FOLDER/build/stride/"
+fstr="$BINARY start --home $STRIDE_FOLDER"
 
 launch_file=$INSTALL_FOLDER/launch_poolparty.sh
-
+rm -f $launch_file
 echo $fstr >> $launch_file
+printf $BLINE
+printf "\n\n"
+echo "You're all done! You can now launch your node with the following command:"
+echo "     ${PURPLE}strided start${NC}"
+echo "Or, if you'd prefer:"
+echo "     ${PURPLE}sh $launch_file${NC}"
+echo "Just make sure $BINARY_LOCATION is in your PATH."
 
+sleep 2
+
+printf "\nNow for the fun part.\n\n"
 while true; do
-    read -p "Do you want to launch your blockchain? [y/n] " yn
+    read -p "$(echo $PURPLE"Do you want to launch your blockchain? [y/n] "$NC)" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
