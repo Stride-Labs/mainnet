4aecb80e216ef8ba8203a218a87aa175667d47ce
Dragon <icodragon@vadim.ospd.net>
2022-07-04 16:29:03 +1000

    Added systemd service file

--- poolparty/init_poolparty.sh
+++ poolparty/init_poolparty.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 clear 
 
-SCRIPT_VERSION="v0.0.5"
+SCRIPT_VERSION="v0.0.12"
 
 # you can always install this script with
 # curl -L install.poolparty.stridelabs.co | sh
@@ -13,20 +13,42 @@ BLUE='\033[1;34m'
 ITALIC="\033[3m"
 NC="\033[0m"
 
-STRIDE_COMMIT_HASH=7593986816487e3f1f1fe6ea13516ebbdabba257
-GENESIS_URL=https://bafkreih4n2wgyxabnqbtq2ve2xyx433ymcr6fidxmrs5vdzxudmqgkmzt4.ipfs.dweb.link/
-PERSISTENT_PEER_ID="d06facbaf30e2b58a97410f48a66db975ceaed68@stride-node1.internal.stridenet.co:26656"
+STRIDE_COMMIT_HASH=2e18074d72468eccf65f34f5714fdf2b4d5f4db3
+GENESIS_URL=https://bafkreihd4q32o2vxzprjnaz5pwv2ptjtlibeektr4tnxnurjlhmtft2c5m.ipfs.dweb.link/
+PERSISTENT_PEER_ID="c79f0058eb25e9290d79e1a7c5a473ab96ef1a6f@stride-node1.internal.stridenet.co:26656"
 
 printf "\n\n${BOLD}Welcome to the setup script for Stride's Testnet, ${PURPLE}PoolParty${NC}!\n\n"
 printf "This script will guide you through setting up your very own Stride node locally.\n"
 printf "You're currently running $BOLD$SCRIPT_VERSION$NC of the setup script.\n\n"
 
-printf "First, we need to give your node a nickname. "
+printf "Before we begin, let's make sure you have all the required dependencies installed.\n"
+DEPENDENCIES=( "git" "go" "jq" )
+missing_deps=false
+for dep in ${DEPENDENCIES[@]}; do
+    printf "\t%-7s" "$dep..."
+    if [[ $(type $dep 2> /dev/null) ]]; then
+        printf "$BLUE\xE2\x9C\x94$NC\n" # checkmark
+    else
+        missing_deps=true
+        printf "$PURPLE\xE2\x9C\x97$NC\n" # X
+    fi
+done
+if [[ $missing_deps = true ]]; then
+    printf "\nPlease install all required dependencies and rerun this script!\n"
+    exit 1
+fi
+
+printf "\nAwesome, you're all set.\n"
+
+BLINE="\n${BLUE}============================================================================================${NC}\n"
+printf $BLINE
+
+printf "\nNext, we need to give your node a nickname. "
 
 node_name_prompt="What would you like to call it? "
 while true; do
-    read -p "$(echo $PURPLE"$node_name_prompt"$NC)" NODE_NAME
-    if [ -z "$NODE_NAME" ] || ! [[ "$NODE_NAME" =~ ^[A-Za-z0-9-]*$ ]]; then
+    read -p "$(printf $PURPLE"$node_name_prompt"$NC)" NODE_NAME
+    if [[ ! "$NODE_NAME" =~ ^[A-Za-z0-9-]+$ ]]; then
         printf '\nNode names can only container letters, numbers, and hyphens.\n'
         node_name_prompt="Please enter a new name. "
     else
@@ -50,11 +72,11 @@ then
     printf "Proceed carefully, because you won't be able to recover your data if you overwrite it.\n\n"
     pstr="Do you want to overwrite your existing $TESTNET installation and proceed? [y/n] "
     while true; do
-        read -p "$(echo $PURPLE"$pstr"$NC)" yn
+        read -p "$(printf $PURPLE"$pstr"$NC)" yn
         case $yn in
             [Yy]* ) break;;
             [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.\n";;
+            * ) printf "Please answer yes or no.\n";;
         esac
     done
 fi
@@ -66,27 +88,28 @@ rm -f launch_testnet.sh > /dev/null 2>&1
 mkdir -p $INSTALL_FOLDER
 cd $INSTALL_FOLDER
 
-echo "\nFetching Stride's code...\n"
+printf "\nFetching Stride's code...\n"
 git clone https://github.com/Stride-Labs/stride.git > /dev/null 2>&1
 cd $INSTALL_FOLDER/stride 
 git checkout $STRIDE_COMMIT_HASH > /dev/null 2>&1
 
 # pick install location
 DEFAULT_BINARY="$HOME/go/bin"
+printf "\nAlmost there! "
 rstr="Where do you want to install your stride binary? [default: $DEFAULT_BINARY] "
-read -p "$(echo Almost there\! $PURPLE"$rstr"$NC)" BINARY_LOCATION
+read -p "$(printf $PURPLE"$rstr"$NC)" BINARY_LOCATION
 if [ -z "$BINARY_LOCATION" ]; then
     BINARY_LOCATION=$DEFAULT_BINARY
 fi
-printf "\n"
 mkdir -p $BINARY_LOCATION
-sh $INSTALL_FOLDER/stride/scripts-local/build.sh -s $BINARY_LOCATION
+go build -mod=readonly -trimpath -o $BINARY_LOCATION ./... > /dev/null 2>&1
 printf "\n"
 
 printf $BLINE
 
 BINARY=$BINARY_LOCATION/strided
-printf "\nLast step, we need to setup your genesis state to match PoolParty...\n"
+printf "\nLast step, we need to setup your genesis state to match PoolParty.\n"
+printf "\nDownloading data from IPFS...\n"
 
 $BINARY init $NODE_NAME --home $STRIDE_FOLDER --chain-id STRIDE --overwrite > /dev/null 2>&1
 
@@ -95,19 +118,22 @@ curl $GENESIS_URL -o $STRIDE_FOLDER/config/genesis.json > /dev/null 2>&1
 
 # # add persistent peer
 config_path="$STRIDE_FOLDER/config/config.toml"
-app_path="$STRIDE_FOLDER/config/app.toml"
 sed -i -E "s|persistent_peers = \".*\"|persistent_peers = \"$PERSISTENT_PEER_ID\"|g" $config_path
 
 # fetch state sync params
 fetched_state="$(curl -s https://stride-node3.$TESTNET.stridenet.co:445/commit | jq "{height: .result.signed_header.header.height, hash: .result.signed_header.commit.block_id.hash}")"
 height="$(echo $fetched_state | jq -r '.height')"
 hash="$(echo $fetched_state | jq -r '.hash')"
-sed -i -E "s|enable = false|enable = true|g" $app_path
-sed -i -E "s|trust_height = 0|trust_height = $height|g" $app_path
-sed -i -E "s|trust_hash = \"\"|trust_hash = \"$hash\"|g" $app_path
-sed -i -E "s|trust_period = \"168h0m0s\"|trust_period = \"3600s\"|g" $app_path
+echo "HASH"
+echo $hash
+echo "HEIGHT"
+echo $height
+sed -i -E "s|enable = false|enable = true|g" $config_path
+sed -i -E "s|trust_height = 0|trust_height = $height|g" $config_path
+sed -i -E "s|trust_hash = \"\"|trust_hash = \"$hash\"|g" $config_path
+sed -i -E "s|trust_period = \"168h0m0s\"|trust_period = \"3600s\"|g" $config_path
 statesync_rpc="stride-node2.$TESTNET.stridenet.co:26657,stride-node3.$TESTNET.stridenet.co:26657"
-sed -i -E "s|rpc_servers = \"\"|rpc_servers = \"$statesync_rpc\"|g" $app_path
+sed -i -E "s|rpc_servers = \"\"|rpc_servers = \"$statesync_rpc\"|g" $config_path
 
 fstr="$BINARY start --home $STRIDE_FOLDER"
 
@@ -116,22 +142,22 @@ rm -f $launch_file
 echo $fstr >> $launch_file
 printf $BLINE
 printf "\n\n"
-echo "You're all done! You can now launch your node with the following command:\n"
-echo "     ${PURPLE}strided start${NC}\n"
-echo "Or, if you'd prefer:\n"
-echo "     ${PURPLE}sh $launch_file${NC}\n"
-echo "Just make sure $BINARY_LOCATION is in your PATH."
+printf "You're all done! You can now launch your node with the following command:\n\n"
+printf "     ${PURPLE}strided start${NC}\n\n"
+printf "Or, if you'd prefer:\n\n"
+printf "     ${PURPLE}sh $launch_file${NC}\n\n"
+printf "Just make sure $BINARY_LOCATION is in your PATH."
 
 sleep 2
-printf "\nNow for the fun part.\n\n"
+printf "\n\nNow for the fun part.\n\n"
 sleep 2
 
 while true; do
-    read -p "$(echo $PURPLE"Do you want to launch your blockchain? [y/n] "$NC)" yn
+    read -p "$(printf $PURPLE"Do you want to launch your blockchain? [y/n] "$NC)" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
+        * ) printf "Please answer yes or no.\n";;
     esac
 done
 
@@ -144,5 +170,28 @@ lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
 PORT_NUMBER=26557
 lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
 
+read -p "Create a systemd service file to run the node? [y/n] " is_create_systemd_file
+if [[ "$is_create_systemd_file" =~ ^([Yy])$ ]]
+then
+    tee /etc/systemd/system/strided.service > /dev/null <<EOF
+[Unit]
+Description=Strided Node
+After=network.target
 
-sh $launch_file
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which strided) start
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    systemctl daemon-reload && \
+    systemctl enable strided.service && \
+    systemctl restart strided.service && \
+    journalctl -u strided.service -f -o cat
+else
+    sh $launch_file
+fi
