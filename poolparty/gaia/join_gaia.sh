3e7b5aef940fc5ecdf4a34044bd2d0e4000f76ed
vish-stride <vishal@stridelabs.co>
2022-07-19 23:15:57 -0400

    added nginx config

--- poolparty/gaia/join_gaia.sh
+++ poolparty/gaia/join_gaia.sh
@@ -133,7 +133,11 @@ sed -i -E "s|trust_hash = \"\"|trust_hash = \"$hash\"|g" $config_path
 sed -i -E "s|trust_period = \"168h0m0s\"|trust_period = \"3600s\"|g" $config_path
 statesync_rpc="gaia.$TESTNET.stridenet.co:26657,gaia.$TESTNET.stridenet.co:26657"
 sed -i -E "s|rpc_servers = \"\"|rpc_servers = \"$statesync_rpc\"|g" $config_path
-sed -i -E 's|unsafe-cors = false|unsafe-cors = true|g' $app_path
+# sed -i -E 's|unsafe-cors = false|unsafe-cors = true|g' $app_path
+sed -i -E 's|cors_allowed_origins = []|cors_allowed_origins = ["*"]|g' $app_path
+sed -i -E 's|enable = false|enable = true|g' $app_path
+sed -i -E 's|127.0.0.1|0.0.0.0|g' $app_path
+
 
 fstr="$BINARY start --home $GAIA_FOLDER"
 
